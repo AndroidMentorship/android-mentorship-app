1ccd8e7bf432dec3de53db929fc91512bb2f2187
damomos <damomos123@gmail.com>
2018-05-05 15:29:08 +0100

    Enabled no action bar and full screen

--- app/src/main/java/com/am/fdamilola/bankingapp/base/classes/TinyGameAppBaseActivity.java
+++ app/src/main/java/com/am/fdamilola/bankingapp/base/classes/TinyGameAppBaseActivity.java
@@ -3,6 +3,7 @@ package com.am.fdamilola.bankingapp.base.classes;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.am.fdamilola.bankingapp.debug.utils.DebugLog;
 
@@ -16,6 +17,10 @@ public abstract class TinyGameAppBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Enables full screen view
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.logMessage(" onCreate -> ".concat(this.getClassName()));
         this.isReady = true;
     }
