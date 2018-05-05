9ece987b1481b267ee89ed5c2a9eb2c2da6f6752
damomos <damomos123@gmail.com>
2018-05-05 01:49:17 +0100

    Designed the XML for the Menu Screen

---
+++ app/src/main/java/com/am/fdamilola/bankingapp/activities/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.am.fdamilola.bankingapp.activities;
+
+import android.os.Bundle;
+
+import com.am.fdamilola.bankingapp.R;
+import com.am.fdamilola.bankingapp.base.classes.BankingAppBaseActivity;
+
+
+public class MenuActivity extends BankingAppBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
---
+++ app/src/main/java/com/am/fdamilola/bankingapp/presenter/menuactivity/MenuActivityPresenter.java
@@ -0,0 +1,8 @@
+package com.am.fdamilola.bankingapp.presenter.menuactivity;
+
+/**
+ * Created by Princess on 02/05/2018.
+ */
+
+public interface MenuActivityPresenter {
+}
