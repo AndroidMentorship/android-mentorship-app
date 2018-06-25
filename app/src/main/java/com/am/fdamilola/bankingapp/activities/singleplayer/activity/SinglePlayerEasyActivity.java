ba8b71d1791bbfe960b95d56d3d96b542cc07d1f
Noel-96 <noel201296@gmail.com>
2018-06-25 10:13:17 +0100

    Single Player Easy Activity logic implemented

--- app/src/main/java/com/am/fdamilola/bankingapp/activities/singleplayer/activity/SinglePlayerEasyActivity.java
+++ app/src/main/java/com/am/fdamilola/bankingapp/activities/singleplayer/activity/SinglePlayerEasyActivity.java
@@ -1,9 +1,12 @@
 package com.am.fdamilola.bankingapp.activities.singleplayer.activity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.am.fdamilola.bankingapp.R;
 import com.am.fdamilola.bankingapp.activities.singleplayer.activity.presenter.SinglePlayerEasyActivityPresenterImpl;
@@ -15,7 +18,14 @@ import butterknife.OnClick;
 
 public class SinglePlayerEasyActivity extends TinyGameAppBaseActivity {
 
-    private static int  buttonCount = 0;
+    private int  buttonCount = 0;
+
+    private int buttoncolor1 = 0;
+    private int buttoncolor2 = 0;
+    private int buttoncolor3 = 0;
+
+    private static int SPLASH_TIME_OUT = 5002;
+    private  final Handler handler = new Handler();
 
     @BindView(R.id.color_view_one) public View colorViewOne;
     @BindView(R.id.color_view_two) public View colorViewTwo;
@@ -45,12 +55,65 @@ public class SinglePlayerEasyActivity extends TinyGameAppBaseActivity {
         setContentView(R.layout.activity_single_player_easy);
         presenter = SinglePlayerEasyActivityPresenterImpl.createSingleInstance(new SinglePlayerEasyActivityVM(this, null));
         presenter.playRoulette();
-       //presenter.setWhiteColor();
+        //presenter.setWhiteColor();
+
+        if (buttonCount == 3 || buttonCount>=3){
+            presenter.playRoulette();
+                       if(buttoncolor1 == ((ColorDrawable) colorViewOne.getBackground()).getColor()||
+                          buttoncolor2 == ((ColorDrawable) colorViewOne.getBackground()).getColor()||
+                          buttoncolor3 == ((ColorDrawable) colorViewOne.getBackground()).getColor()){
+
+                           if(buttoncolor1 == ((ColorDrawable) colorViewTwo.getBackground()).getColor()||
+                                   buttoncolor2 == ((ColorDrawable) colorViewTwo.getBackground()).getColor()||
+                                   buttoncolor3 == ((ColorDrawable) colorViewTwo.getBackground()).getColor()){
+
+                               if(buttoncolor1 == ((ColorDrawable) colorViewThree.getBackground()).getColor()||
+                                       buttoncolor2 == ((ColorDrawable) colorViewThree.getBackground()).getColor()||
+                                       buttoncolor3 == ((ColorDrawable) colorViewThree.getBackground()).getColor()){
+                                   //Trigger dami Success dialog
+                                   Toast.makeText(this,"you got the right answer",Toast.LENGTH_SHORT);
+                               }else{
+                                   //Trigger dami failure dialog
+                                   Toast.makeText(this,"you failed to get the right answer",Toast.LENGTH_SHORT);
+                               }
+
+                           }else{
+                               //Trigger dami failure dialog
+                               Toast.makeText(this,"you failed to get the right answer",Toast.LENGTH_SHORT);
+                           }
+              }else{
+                           //Trigger dami failure dialog
+                           Toast.makeText(this,"you failed to get the right answer",Toast.LENGTH_SHORT);
+                       }
+        }
     }
 
-    @OnClick(R.id.button0_0)
-    public void checkButton(View view) {
-        ColorDrawable buttonColor = (ColorDrawable) buttonRow0col0.getBackground();
-        buttonCount +=1;
+    @OnClick({R.id.button0_0,R.id.button0_1,R.id.button0_2,R.id.button0_3,R.id.button0_4,
+              R.id.button1_0,R.id.button1_1,R.id.button1_2,R.id.button1_3,R.id.button1_4,
+              R.id.button2_0,R.id.button2_1,R.id.button2_2,R.id.button2_3,R.id.button2_4})
+    public void checkButton(Button button) {
+        if (buttoncolor1 != 0){
+            if (buttoncolor2!=0){
+                if (buttoncolor3!=0){
+                    Toast.makeText(this,"you have already selected three buttons",Toast.LENGTH_SHORT);
+                }else if (buttoncolor3 == 0){
+                    buttoncolor3= ((ColorDrawable)button.getBackground()).getColor();
+                    buttonCount +=1;
+                    Toast.makeText(this,"oh boy you don click button 3",Toast.LENGTH_LONG);
+                }
+            }else if (buttoncolor2 == 0){
+                buttoncolor2= ((ColorDrawable)button.getBackground()).getColor();
+                buttonCount +=1;
+                Toast.makeText(this,"oh boy you don click button 2",Toast.LENGTH_LONG);
+            }
+        }else if (buttoncolor1 ==0){
+            buttoncolor1= ((ColorDrawable)button.getBackground()).getColor();
+            buttonCount +=1;
+            Toast.makeText(this,"oh boy you don click button 1",Toast.LENGTH_LONG);
+        }
     }
+
+
+
+
 }
